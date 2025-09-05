000f75d6eba1c6a0cdf6532996dd7ffa80034549
Muhammad Arslan <muhammadarslan30000@gmail.com>
2025-09-05 20:49:23 +0500

    first screen layout made

---
+++ app/src/main/java/com/example/assigment_1/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.assigment_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
