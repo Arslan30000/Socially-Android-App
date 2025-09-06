2e961869f3379c08d6b41bd1032aae069bd8fa00
Muhammad Arslan <muhammadarslan30000@gmail.com>
2025-09-06 15:18:22 +0500

    Second and Third Screen have been completed both handle button logic

---
+++ app/src/main/java/com/example/assigment_1/FourthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.assigment_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class FourthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_2)
+
+    }
+}
--- app/src/main/java/com/example/assigment_1/ThirdActivity.kt
+++ app/src/main/java/com/example/assigment_1/ThirdActivity.kt
@@ -4,25 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class SecondActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up)
-
-
-        val backBtn: ImageButton = findViewById(R.id.back_arrow)
+        setContentView(R.layout.login)
+        val backBtn: TextView = findViewById(R.id.sign_up_button)
         backBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
             finish()
         }
-
-        val makeAccountBtn: ImageButton = findViewById(R.id.sign_in_icon)
-        makeAccountBtn.setOnClickListener {
-            val intent = Intent(this, ThirdActivity::class.java)
+        val nextbtn: ImageButton = findViewById(R.id.login_button)
+        nextbtn.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
