c6b9881ed9d03ef159aa288bc7034b551f3ff617
Muhammad Arslan <muhammadarslan30000@gmail.com>
2025-09-09 19:20:40 +0500

    12th completed

--- app/src/main/java/com/example/assigment_1/TwelfthActivity.kt
+++ app/src/main/java/com/example/assigment_1/TwelfthActivity.kt
@@ -2,15 +2,20 @@ package com.example.assigment_1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class EleventhActivity: AppCompatActivity() {
+class TwelfthActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recent_activity)
+        setContentView(R.layout.your_activity)
+        val follow_btn: RelativeLayout = findViewById(R.id.following_tab)
+        follow_btn.setOnClickListener {
+            val intent = Intent(this, EleventhActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
         val homebtn: ImageView = findViewById(R.id.home_icon)
         homebtn.setOnClickListener {
             val intent = Intent(this, FifthActivity::class.java)
@@ -23,12 +28,7 @@ class EleventhActivity: AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        val you_button: TextView = findViewById(R.id.tab_you)
-        you_button.setOnClickListener {
-            val intent = Intent(this, TwelfthActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
 
 }
