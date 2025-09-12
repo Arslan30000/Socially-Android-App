3dfc43738f999c594bd22dbf04cea431f26d746d
Muhammad Arslan <muhammadarslan30000@gmail.com>
2025-09-12 19:27:38 +0500

    17 pages done

--- app/src/main/java/com/example/assigment_1/FifthActivity.kt
+++ app/src/main/java/com/example/assigment_1/FifthActivity.kt
@@ -2,6 +2,8 @@ package com.example.assigment_1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 class FifthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,31 +14,73 @@ class FifthActivity : AppCompatActivity() {
         Searchbtn.setOnClickListener {
             val intent = Intent(this, SixthActivity::class.java)
             startActivity(intent)
-            finish()
+
         }
         val messagebtn: ImageView = findViewById(R.id.share)
         messagebtn.setOnClickListener {
             val intent = Intent(this, EightAvtivity::class.java)
             startActivity(intent)
-            finish()
+
         }
         val like_btn: ImageView = findViewById(R.id.like_icon)
         like_btn.setOnClickListener {
             val intent = Intent(this, EleventhActivity::class.java)
             startActivity(intent)
-            finish()
+
         }
         val create_post_btn: ImageView = findViewById(R.id.post_icon)
         create_post_btn.setOnClickListener {
             val intent = Intent(this, FifteenthActivity::class.java)
             startActivity(intent)
-            finish()
+
         }
         val camera_btn: ImageView = findViewById(R.id.camera)
         camera_btn.setOnClickListener {
             val intent = Intent(this, SixteenActivity::class.java)
             startActivity(intent)
-            finish()
+
+        }
+        val story_btn_1: LinearLayout  = findViewById(R.id.s_1)
+        story_btn_1.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
+        }
+        val story_btn_2: LinearLayout  = findViewById(R.id.s_2)
+        story_btn_2.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
+        }
+        val story_btn_3: LinearLayout  = findViewById(R.id.s_3)
+        story_btn_3.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
+        }
+        val story_btn_4: LinearLayout  = findViewById(R.id.s_4)
+        story_btn_4.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
+        }
+        val story_btn_5: LinearLayout = findViewById(R.id.s_5)
+        story_btn_5.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
+        }
+        val story_btn_6: LinearLayout = findViewById(R.id.s_6)
+        story_btn_6.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
+        }
+        val story_btn_7: LinearLayout = findViewById(R.id.s_7)
+        story_btn_7.setOnClickListener {
+            val intent = Intent(this, SeventeenActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
---
+++ app/src/main/java/com/example/assigment_1/SeventeenActivity.kt
@@ -0,0 +1,18 @@
+package com.example.assigment_1
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+class SeventeenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.story_page)
+        val close_btn: ImageView = findViewById(R.id.close)
+        close_btn.setOnClickListener {
+            val intent = Intent(this, SeventhActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
